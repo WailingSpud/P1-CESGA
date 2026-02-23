8386cc7a4d7dfd48e7b66e4765c5fbe815f97451
WailingSpud <sebastianmontotop@rai.usc.es>
2026-02-23 12:13:27 +0100

    Comentando

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "rutinasCiclos.c"
 
-//L1d=3 MiB = 3,145,728 L1i = 2 MiB = 2,097,152 L1 = 5,242,880 TamLinea=64B       L2=80 MiB = 83,886,080          1,048,576 bytes
+//L1d=3 MiB = 3,145,728 L1i = 2 MiB = 2,097,152 L1 = 5,242,880 TamLinea=64B       L2=80 MiB = 83,886,080          1MiB = 1,048,576 bytes
 int S1= 81920;
 int S2= 1310720;
 
@@ -11,22 +11,23 @@ int D[5] = []; //Salto (5 potencias de 2)
 
 int main(int argc, char *argv[]) {
     double *A;
-    double S[9];
+    double S[9]; //Array de Resultados
     for(int k=0;k<10;k++){
-        A = (double*)aligned_alloc(1024, 1024 * sizeof(double));
-        double* P = &A[0];
         double s;
         for(int i=0;i<5;i++){
             int dactual = D[i];
             for(int j=0;j<7;j++){
                 int ractual = R[j];
                 int ind[ractual];
+                A = (double*)aligned_alloc(1024, 1024 * sizeof(double)); //Vector variable con R y D
+                double* P = &A[0]; //Puntero al primer elemento del array
                 for(int r=0;r<ractual;r++){
-                    ind[r] = r*dactual;
+                    ind[r] = r*dactual; //Array de índices de N*D
                 }
                 for(int r=0;r<ractual;r++){
-                    s += A[ind[r]];
+                    s += A[ind[r]]; //Suma
                 }
+                free(P); //Libera cada array al final
             }
         }
         S[k] = s;
